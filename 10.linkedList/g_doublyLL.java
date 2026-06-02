4772771f779f4d281bdbc38e4496f140f60b79be
Harpreet singh <harpreet9singh2019@gmail.com>
2026-06-02 22:16:27 +0530

    new

---
+++ 10.linkedList/g_doublyLL.java
@@ -0,0 +1,61 @@
+public class g_doublyLL {
+    public static class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public static int removeFirst(){
+        if(head == null){
+            System.out.println("ll is empty");
+            return -1;
+        }
+        if(head.next == null){
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        return val;
+    }
+
+    public static void reverse(){
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+            prev = curr;
+            curr = next;
+        }
+    }
+    public static void main(String[] args) {
+        g_doublyLL dll = new g_doublyLL();
+    }
+}
